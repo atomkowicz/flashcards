6cd66c5ccd256048bf3581243af59deb2e431326
Agnieszka Tomkowicz <agntomk@gmail.com>
2017-11-27 18:13:43 +0100

    Add styles

--- components/Deck.js
+++ components/Deck.js
@@ -1,15 +1,35 @@
-import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import {purple, gray, white} from '../utils/colors';
 
 class Deck extends Component {
     render() {
         const { title } = this.props
         return (
             <View>
-                <Text>Deck: {title}</Text>
+                <View style={styles.deck}>
+                <Text style={styles.text}>Deck: {title}</Text>
+                </View>
             </View>
         )
     }
 }
 
 export default Deck;
+
+const styles = StyleSheet.create({
+    deck: {
+      flex: 1,
+      marginTop: 12,
+      padding: 40,
+      backgroundColor: white,
+      shadowColor: 'rgba(0, 0, 0, 0.24)',
+      shadowOffset: {
+        width: 0,
+        height: 3
+      },
+    },
+    text: {
+        fontSize: 20        
+    }
+  })
